1b54297641ddc12d583755ce6b45365e04fb3973
Stutommi <tommi.teetee@hotmail.com>
2019-04-19 10:07:38 +0300

    ready to deploy

---
+++ build/precache-manifest.b8333201879340cfb57523ccdbe88f05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0897bef079ad7a81701b",
+    "url": "/static/js/main.bbdc1d54.chunk.js"
+  },
+  {
+    "revision": "37cfae212e25180f959b",
+    "url": "/static/js/2.0b74813f.chunk.js"
+  },
+  {
+    "revision": "0897bef079ad7a81701b",
+    "url": "/static/css/main.3e8a4cd8.chunk.css"
+  },
+  {
+    "revision": "ca07a87d2c6ff83ca3b1c1223c6a1347",
+    "url": "/index.html"
+  }
+];
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -8,6 +9,7 @@ morgan.token('body', (req) => {
     return JSON.stringify(req.body)
 })
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(morgan(':method :url :status :res[content-length] :res[header] :response-time ms :body'))
 
@@ -82,7 +84,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
